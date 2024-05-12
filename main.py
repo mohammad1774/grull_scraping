94b6e8325bbf25b0637148af0312a682aefa8222
mohammad1774 <mohammad.ahmed1774@gmail.com>
2024-05-12 17:58:41 +0530

    code correction

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from requests_html import HTMLSession
 from datetime import datetime
 import gspread
 import pickle
+import logging
+import logging.handlers
+import os
+
 
 def stock_prices_coinswitch():
     tabular_data = []
@@ -109,9 +113,30 @@ def stock_prices_wazirx():
         return t_data[:20]
 
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "status.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
+
+try:
+    SOME_SECRET = os.environ["SOME_SECRET"]
+except KeyError:
+    SOME_SECRET = "Token not available!"
+    #logger.info("Token not available!")
+    #raise
+
+
 
 
 if __name__ == '__main__':
+    logger.info(f"Token value: {SOME_SECRET}")
 
     sa = gspread.service_account(filename='google_action.json')
     

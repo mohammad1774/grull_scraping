85d386306bbdbc5d8d2f78f1583532ba475398cc
mohammad1774 <mohammad.ahmed1774@gmail.com>
2024-05-13 04:18:22 +0530

    trying with scrapfly

--- main.py
+++ main.py
@@ -5,78 +5,9 @@ import gspread
 import pickle
 import requests
 import time
+from scrapfly import ScrapflyClient, ScrapeConfig, ScrapeApiResponse
 
-def stock_prices_coinswitch():
-    tabular_data = []
-    url = 'https://coinswitch.co/coins'
-    session = HTMLSession()
-    response = session.get(url)
-    present_time = datetime.now()
-    present_time = present_time.isoformat()
-    website = 'Coin Switch'
-    try:
-        if response.status_code == 200:
-            response.html.render()
-
-            soup = BeautifulSoup(response.html.html,'lxml')
-            table = soup.find('div',class_='cdt-prices-container')
-            table_container = table.find('div',class_='small-price-container').find('div',class_='price-chart').find('div',class_='coinlist-container')
-            rows = []
-            for i in range(20):
-                row = table_container.find('div',class_=f'cdt-trends-div-map switch{i}')
-                rows.append(row)
-                #print('\n\n\n',row.text)
-                name = row.find('div',class_='cdt-trends-left').find('div',class_ = '').text
-                price = row.find('div',class_ = 'cdt-trends-right').find('div',class_='cdt-trends-top').text
-                clean_row = [name,'₹'+price,website,present_time]
-                tabular_data.append(clean_row)
-            return tabular_data[:20]
-        else:
-            print('connection not successfull.')
-            t_data = stock_prices_coinswitch()
-            return t_data
-
-    except:
-        time.sleep(2)
-        print('connection error coinswitch')
-        t_data  = stock_prices_coinswitch()
-        return t_data
-
-def stock_price_coinswitch():
-    tabular_data = []
-    url = 'https://coinswitch.co/coins/'
-    session = HTMLSession()
-    response = session.get(url)
-    present_time = datetime.now()
-    present_time = present_time.isoformat()
-    website = 'Coin Switch'
-    try:
-        if response.status_code == 200:
-            response.html.render()
-
-            soup = BeautifulSoup(response.html.html,'lxml')
-            table = soup.find('div',class_='cdt-prices-container')
-            table_container = table.find('div',class_='small-price-container').find('div',class_='price-chart').find('div',class_='coinlist-container')
-            rows = []
-            for i in range(20):
-                row = table_container.find('div',class_=f'cdt-trends-div-map switch{i}')
-                rows.append(row)
-                #print('\n\n\n',row.text)
-                name = row.find('div',class_='cdt-trends-left').find('div',class_ = '').text
-                price = row.find('div',class_ = 'cdt-trends-right').find('div',class_='cdt-trends-top').text
-                clean_row = [name,'₹'+price,website,present_time]
-                tabular_data.append(clean_row)
-            return tabular_data[:20]
-        else:
-            print('connection not successfull.')
-            t_data = stock_prices_coinswitch()
-            return t_data
-
-    except AttributeError:
-        print('connection error')
-        t_data  = stock_prices_coinswitch()
-        return t_data
-
+# 
 def stock_prices_mudrex():
     url = 'https://mudrex.com/all-cryptocurrencies'
     tabular_data = []
@@ -114,41 +45,6 @@ def stock_prices_mudrex():
         return t_data
     
 
-def stock_price_mudrex():
-    url = 'https://mudrex.com/all-cryptocurrencies'
-    tabular_data = []
-
-
-    session = HTMLSession()
-    response = session.get(url)
-    response.html.render()
-
-    try:
-        if response.status_code == 200:
-            response.html.render()
-            present_time = datetime.now()
-            present_time = present_time.isoformat()
-            soup = BeautifulSoup(response.html.html, 'lxml')
-            table1 = soup.find('div',class_='w-full overflow-x-auto').find('table')
-            tbody = table1.find('tbody',class_='table-contents')
-            for row in tbody.find_all('tr'):
-                #print(row,'\n',row.text,'\n\n\n')
-                name = row.find('a').text
-                price = row.find('td',class_ = 'p-6 font-medium h-8 text-sm align-middle text-right').text
-                row_data = [name,price,'Mudrex',present_time]
-                #print(row_data)
-                tabular_data.append(row_data)
-            return tabular_data
-        else:
-            print('connection not successful.')
-            t_data = stock_prices_mudrex()
-            return t_data
-           # print(tbody.text)
-    except :
-        t_data = stock_prices_mudrex()
-        return t_data
-
-
 def stock_prices_wazirx():
     url = 'https://wazirx.com/exchange/BTC-INR'
 
@@ -184,6 +80,79 @@ def stock_prices_wazirx():
         return t_data[:20]
 
 
+def stock_prices_coindcx():
+    api = 'scp-live-b27b2bd698bb49b0a43b9b6291f0acda'
+    scrapfly = ScrapflyClient(key=api)
+
+    result = scrapfly.scrape(
+    ScrapeConfig(
+        url = 'https://coindcx.com/trade/BTCINR',
+        country = 'in',
+        proxy_pool = 'public_residential_pool',
+        render_js=True,
+        asp = True,
+         js_scenario=[
+        {"click": {"selector": "button.-c-tab--strong.white--text","ignore_if_not_visible": False}}]
+    )
+)
+
+    try:
+        tabular_data = []
+        table = result.soup.find('div',class_ = 'currency-list__container').find_all('a')
+        #print(table)
+
+        present_time = datetime.now()
+        present_time  = present_time.isoformat()
+        for row in table:
+            name = row.find('div',class_='info').find('div',class_= 'name__container stat--strong').find('span').get_text()
+        #    print(name)
+            price = row.find('div',class_="price -c-show-on-portrait").find('p',class_= 'converted-price stat').get_text()
+        #    print(price,'\n\n')
+            row_data = [name,price,'CoinDCX',present_time]
+            tabular_data.append(row_data)
+        return tabular_data[:20]
+    except Exception as e:
+        print('There is an error: ',e)
+
+
+def stock_prices_coinswitch():
+    api = 'scp-live-b27b2bd698bb49b0a43b9b6291f0acda'
+    scrapfly = ScrapflyClient(key=api)
+
+    result = scrapfly.scrape(
+    ScrapeConfig(
+        url = 'https://coinswitch.co/coins',
+        country = 'in',
+        proxy_pool = 'public_residential_pool',
+        render_js=True,
+        asp = True
+    )
+)
+    
+    present_time = datetime.now()
+    present_time = present_time.isoformat()
+    website = 'Coin Switch'
+    try:
+        tabular_data = []
+
+        table = result.soup.find('div',class_='cdt-prices-container')
+#        table = soup.find('div',class_='cdt-prices-container')
+        table_container = table.find('div',class_='small-price-container').find('div',class_='price-chart').find('div',class_='coinlist-container')
+        rows = []
+        for i in range(20):
+            row = table_container.find('div',class_=f'cdt-trends-div-map switch{i}')
+            rows.append(row)
+            #print('\n\n\n',row.text)
+            name = row.find('div',class_='cdt-trends-left').find('div',class_ = '').text
+            price = row.find('div',class_ = 'cdt-trends-right').find('div',class_='cdt-trends-top').text
+            clean_row = [name,'₹'+price,website,present_time]
+            tabular_data.append(clean_row)
+        return tabular_data[:20]
+    except:
+        time.sleep(2)
+        print('connection error coinswitch')
+        t_data  = stock_prices_coinswitch()
+        return t_data
 
 
 if __name__ == '__main__':
@@ -195,19 +164,24 @@ if __name__ == '__main__':
     
     table_wazir = stock_prices_wazirx()
     wks.append_rows(table_wazir[:20])
-    print(table_wazir)
+    #print(table_wazir)
 
 
     time.sleep(2)
     table_mudrex = stock_prices_mudrex()
     wks.append_rows(table_mudrex[:20])
-    print(table_mudrex)
+    #print(table_mudrex)
 
     time.sleep(2)
     table_coinswitch = stock_prices_coinswitch()
     wks.append_rows(table_coinswitch[:20])
-    print(table_coinswitch[:20])
+    #print(table_coinswitch[:20])
 
+
+    time.sleep(2)
+    table_coindcx = stock_prices_coindcx()
+    wks.append_rows(table_coindcx)
+    
     wks.append_rows(table_wazir[:20])
 
     #sa = gspread.service_account(filename='grull_round/.config/gspread/service_account.json')
